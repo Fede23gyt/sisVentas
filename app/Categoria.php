7141520dc41c2a3d6bff48710ba2ed26cbfda0bb
Fede23gyt <37566425+Fede23gyt@users.noreply.github.com>
2018-05-15 10:41:01 -0300

    Prueba
    bla blalblblb

--- app/Categoria.php
+++ app/Categoria.php
@@ -10,7 +10,7 @@ class Categoria extends Model
 
     protected $primeryKey='idcategoria';
 
-    /* laravel agrega 2 columnas especificando cuando se ha crado/modificado el registro */
+    /* laravel agrega 2 columnas especificando cuando se ha crado/modificado el registro-Fede */
     public $timestamp=false; 
 
     /* especificamos campos q reciben valor para grabar en BD */
